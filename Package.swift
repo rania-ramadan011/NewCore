264aac0392f2b0f46043c3ae01a64a930a7a39ef
Rania Ramadan <rania.ramadan011@gmail.com>
2024-06-07 17:43:14 +0300

    initial commit

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NewsCore",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NewsCore",
+            targets: ["NewsCore"]),
+    ],
+    dependencies: [
+           .package(url: "https://github.com/rania-ramadan011/NewsApplication.git", from: "1.0.0")
+       ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NewsCore"),
+        .testTarget(
+            name: "NewsCoreTests",
+            dependencies: ["NewsCore"]),
+    ]
+)
---
+++ Sources/NewsCore/Feature/GetHeadlines/Data/Network/HeadlinesAPIEndpoint.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Shaimaa Mohammed on 07/06/2024.
+//
+
+import Foundation
